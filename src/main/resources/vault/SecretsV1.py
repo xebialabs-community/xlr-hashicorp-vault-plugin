382ec1acb6661e7898944b0e89903ad1ff593bbc
Marco Morales <mmorales@xebialabs.com>
2019-12-13 17:04:45 -0500

    more fixes for code quality.

--- src/main/resources/vault/SecretsV1.py
+++ src/main/resources/vault/SecretsV1.py
@@ -1,12 +1,20 @@
 #
 # Copyright 2019 XEBIALABS
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-import hvac
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import logging
 
 from vault import VaultClient
@@ -27,7 +35,7 @@ else:
     vault_client.exit(vault_client.VAULT_NOT_INITIALIZED,
                       "Your Vault Server at {} is not initialized".format(vaultServer['url']))
 
-if (vault_client.client.sys.is_sealed()):
+if vault_client.client.sys.is_sealed():
     vault_client.exit(vault_client.VAULT_SERVER_SEALED, "Vault Server {} is Sealed".format(vaultServer['url']))
 
 if vaultServer['token']:
@@ -55,11 +63,13 @@ elif mytype == 'vault.SecretsV1-ReadDynamicSecret':
     logger.info("ReadDynamic Not Implemented")
 
 elif mytype == 'vault.SecretsV1-CreateSecret':
-    new_secret = {key : value}
+    new_secret = {key: value}
     # TODO: Delete the next line once we know this write operation works.
     logger.info(">> Writing {} to {}".format(new_secret, path))
     print(">> Writing {} to {}".format(new_secret, path))
-    create_response = vault_client.client.secrets.kv.v1.create_or_update_secret(mount_point=mount_point, path=path, secret=new_secret)
+    create_response = vault_client.client.secrets.kv.v1.create_or_update_secret(mount_point=mount_point,
+                                                                                path=path,
+                                                                                secret=new_secret)
     logger.info(">> Write Request complete for {} : {}".format(path, create_response))
     logger.info("===============================================================")
     print(">> Write Request complete for {} : {}".format(path, create_response))
